844ba3382924f54ed94207b819c5f34ecdb4316e
jagreehal <jag.reehal@gmail.com>
2018-04-22 20:28:26 +0100

    added snapshot example

---
+++ src/components/using-snapshots/using-snapshots.tsx
@@ -0,0 +1,17 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'using-snapshots'
+})
+export class UsingSnapshots {
+  @Prop() first: string;
+  @Prop() last: string;
+
+  render() {
+    return (
+      <div>
+        Hello, my name is {this.first} {this.last}
+      </div>
+    );
+  }
+}
