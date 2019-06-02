e41561440110a3a42a83481cb70071bb6dd16508
jagreehal <jag.reehal@gmail.com>
2019-06-03 00:31:54 +0100

    updated examples

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,75 +5,41 @@
  */
 
 
-import '@stencil/core';
-
-
+import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 import {
-  EventEmitter,
-} from '@stencil/core';
+  SomethingHappened,
+} from './components/testing-event-emitter/testing-event-emitter';
 
 
 export namespace Components {
-
+  interface CallbackTest {
+    'clickProp': undefined;
+  }
   interface TestingClickEvent {}
-  interface TestingClickEventAttributes extends StencilHTMLAttributes {}
-
   interface TestingEventEmitter {}
-  interface TestingEventEmitterAttributes extends StencilHTMLAttributes {
-    'onSomethingHappened'?: (event: CustomEvent<SomethingHappened>) => void;
-  }
-
   interface TestingMethods {
-    'incrementCount': () => void;
+    'incrementCount': () => Promise<void>;
   }
-  interface TestingMethodsAttributes extends StencilHTMLAttributes {}
-
   interface TestingProps {
     'first': string;
     'last': string;
   }
-  interface TestingPropsAttributes extends StencilHTMLAttributes {
-    'first'?: string;
-    'last'?: string;
-  }
-
   interface TestingRender {}
-  interface TestingRenderAttributes extends StencilHTMLAttributes {}
-
   interface TestingUpdatesAriaProperty {}
-  interface TestingUpdatesAriaPropertyAttributes extends StencilHTMLAttributes {}
-
   interface UsingSnapshots {
     'first': string;
     'last': string;
   }
-  interface UsingSnapshotsAttributes extends StencilHTMLAttributes {
-    'first'?: string;
-    'last'?: string;
-  }
 }
 
 declare global {
-  interface StencilElementInterfaces {
-    'TestingClickEvent': Components.TestingClickEvent;
-    'TestingEventEmitter': Components.TestingEventEmitter;
-    'TestingMethods': Components.TestingMethods;
-    'TestingProps': Components.TestingProps;
-    'TestingRender': Components.TestingRender;
-    'TestingUpdatesAriaProperty': Components.TestingUpdatesAriaProperty;
-    'UsingSnapshots': Components.UsingSnapshots;
-  }
 
-  interface StencilIntrinsicElements {
-    'testing-click-event': Components.TestingClickEventAttributes;
-    'testing-event-emitter': Components.TestingEventEmitterAttributes;
-    'testing-methods': Components.TestingMethodsAttributes;
-    'testing-props': Components.TestingPropsAttributes;
-    'testing-render': Components.TestingRenderAttributes;
-    'testing-updates-aria-property': Components.TestingUpdatesAriaPropertyAttributes;
-    'using-snapshots': Components.UsingSnapshotsAttributes;
-  }
 
+  interface HTMLCallbackTestElement extends Components.CallbackTest, HTMLStencilElement {}
+  var HTMLCallbackTestElement: {
+    prototype: HTMLCallbackTestElement;
+    new (): HTMLCallbackTestElement;
+  };
 
   interface HTMLTestingClickEventElement extends Components.TestingClickEvent, HTMLStencilElement {}
   var HTMLTestingClickEventElement: {
@@ -116,18 +82,8 @@ declare global {
     prototype: HTMLUsingSnapshotsElement;
     new (): HTMLUsingSnapshotsElement;
   };
-
   interface HTMLElementTagNameMap {
-    'testing-click-event': HTMLTestingClickEventElement
-    'testing-event-emitter': HTMLTestingEventEmitterElement
-    'testing-methods': HTMLTestingMethodsElement
-    'testing-props': HTMLTestingPropsElement
-    'testing-render': HTMLTestingRenderElement
-    'testing-updates-aria-property': HTMLTestingUpdatesAriaPropertyElement
-    'using-snapshots': HTMLUsingSnapshotsElement
-  }
-
-  interface ElementTagNameMap {
+    'callback-test': HTMLCallbackTestElement;
     'testing-click-event': HTMLTestingClickEventElement;
     'testing-event-emitter': HTMLTestingEventEmitterElement;
     'testing-methods': HTMLTestingMethodsElement;
@@ -136,14 +92,47 @@ declare global {
     'testing-updates-aria-property': HTMLTestingUpdatesAriaPropertyElement;
     'using-snapshots': HTMLUsingSnapshotsElement;
   }
-
-
-  export namespace JSX {
-    export interface Element {}
-    export interface IntrinsicElements extends StencilIntrinsicElements {
-      [tagName: string]: any;
-    }
-  }
-  export interface HTMLAttributes extends StencilHTMLAttributes {}
-
 }
+
+declare namespace LocalJSX {
+  interface CallbackTest extends JSXBase.HTMLAttributes<HTMLCallbackTestElement> {
+    'clickProp'?: undefined;
+  }
+  interface TestingClickEvent extends JSXBase.HTMLAttributes<HTMLTestingClickEventElement> {}
+  interface TestingEventEmitter extends JSXBase.HTMLAttributes<HTMLTestingEventEmitterElement> {
+    'onSomethingHappened'?: (event: CustomEvent<SomethingHappened>) => void;
+  }
+  interface TestingMethods extends JSXBase.HTMLAttributes<HTMLTestingMethodsElement> {}
+  interface TestingProps extends JSXBase.HTMLAttributes<HTMLTestingPropsElement> {
+    'first'?: string;
+    'last'?: string;
+  }
+  interface TestingRender extends JSXBase.HTMLAttributes<HTMLTestingRenderElement> {}
+  interface TestingUpdatesAriaProperty extends JSXBase.HTMLAttributes<HTMLTestingUpdatesAriaPropertyElement> {}
+  interface UsingSnapshots extends JSXBase.HTMLAttributes<HTMLUsingSnapshotsElement> {
+    'first'?: string;
+    'last'?: string;
+  }
+
+  interface IntrinsicElements {
+    'callback-test': CallbackTest;
+    'testing-click-event': TestingClickEvent;
+    'testing-event-emitter': TestingEventEmitter;
+    'testing-methods': TestingMethods;
+    'testing-props': TestingProps;
+    'testing-render': TestingRender;
+    'testing-updates-aria-property': TestingUpdatesAriaProperty;
+    'using-snapshots': UsingSnapshots;
+  }
+}
+
+export { LocalJSX as JSX };
+
+
+declare module "@stencil/core" {
+  export namespace JSX {
+    interface IntrinsicElements extends LocalJSX.IntrinsicElements {}
+  }
+}
+
+
