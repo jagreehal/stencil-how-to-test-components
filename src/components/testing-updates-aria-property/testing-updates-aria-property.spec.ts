e8288b3e8a39a457a1af232a5aa35bf0226516fd
jagreehal <jag.reehal@gmail.com>
2019-06-10 21:11:26 +0100

    now using newSpecPage and debug instructions!

--- src/components/testing-updates-aria-property/testing-updates-aria-property.spec.ts
+++ src/components/testing-updates-aria-property/testing-updates-aria-property.spec.ts
@@ -1,7 +1,33 @@
+import { newSpecPage } from '@stencil/core/testing';
 import { TestingUpdatesAriaProperty } from './testing-updates-aria-property';
 
 describe('testing-updates-aria-property', () => {
   it('should build', () => {
     expect(new TestingUpdatesAriaProperty()).toBeTruthy();
+  });
+
+  describe('rendering', () => {
+    let page;
+    let shadowRoot;
+    beforeEach(async () => {
+      page = await newSpecPage({
+        components: [TestingUpdatesAriaProperty],
+        html: '<testing-updates-aria-property></testing-updates-aria-property>',
+        supportsShadowDom: true
+      });
+      shadowRoot = page.root.shadowRoot;
+    });
+
+    it('should be hidden by default', async () => {
+      const content = shadowRoot.querySelector('#content');
+      expect(content.getAttribute('aria-hidden')).toBe('true');
+    });
+
+    it('should NOT be hidden when toggled', async () => {
+      shadowRoot.querySelector('[data-test="toggle"]').click();
+      await page.waitForChanges();
+      const content = shadowRoot.querySelector('#content');
+      expect(content.getAttribute('aria-hidden')).toBe('false');
+    });
   });
 });
